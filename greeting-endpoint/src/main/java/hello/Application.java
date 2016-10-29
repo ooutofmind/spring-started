2a07f2202eea4fa82c9988d22123ab4eaf9d3222
ooutofmind <wofffk@gmail.com>
2016-10-29 23:06:43 +0500

    make deployable to external tomcat

--- greeting-endpoint/src/main/java/hello/Application.java
+++ greeting-endpoint/src/main/java/hello/Application.java
@@ -2,13 +2,20 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * Entry point
  * Created by Vladimir on 29.10.2016.
  */
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
